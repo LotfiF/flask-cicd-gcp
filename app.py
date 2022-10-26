5f1252e28281ca96584178795f06cb3112b4fdbd
Lotfi FATHI <lotfi.fathi@atos.net>
2022-10-26 23:12:34 +0200

    commit n 5

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("Name", "World ! v2")
+    name = os.environ.get("Name", "World ! V2")
     return f"Hello {name}"
 
 def sum(a, b):
